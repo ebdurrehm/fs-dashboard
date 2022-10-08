3cf23af0200de99652c99a4eefd18a7ac09df2d3
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-10-09 00:42:24 +0400

    read  data, filter it and save it to db

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,10 +1,29 @@
-const Planets = [
-  {
-    kepler_name: 'kapler 101'
-  },
-  {
-    kepler_name: 'artox 401'
-  }
-]
+const fs = require('fs')
+const csv = require('csv-parser')
+const path = require('path')
+const planets = require('../../data/models/planet.mongodb.model')
 
-module.exports = Planets
+function isHabitablePlanet (planet) {
+  return planet.koi_disposition === 'CONFIRMED' &&
+  planet.koi_insol > 0.36 && planet.koi_insol < 1.11 &&
+  planet.koi_prad < 1.6
+}
+function deployDataToDB () {
+  fs.createReadStream(path.resolve(__dirname, '..', '..', 'data', 'data.csv'))
+    .pipe(csv({
+      skipComments: true
+    })).on('data', async (data) => {
+      if (isHabitablePlanet(data)) {
+        await planets.updateOne({ keplerName: data.kepler_name }, {
+          keplerName: data.kepler_name
+        }, { upsert: true })
+      }
+    }).on('error', (err) => {
+      console.log(err)
+    }).on('end', async (data) => {
+      const planetSize = (await planets.find({})).length
+      console.log(`${planetSize} habitable planets found`)
+    })
+}
+
+module.exports = deployDataToDB
