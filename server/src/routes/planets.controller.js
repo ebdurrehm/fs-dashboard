67a3fdab855a65c2a030094d4f2fe3a46753f28a
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-10-25 23:53:24 +0400

    refactor planets controller

--- server/src/routes/planets.controller.js
+++ server/src/routes/planets.controller.js
@@ -1,7 +1,7 @@
-const planets = require('../models/planets.model')
+const planets = require('../../data/models/planet.mongodb.model')
 
-function getAllPlanets (req, res) {
-  return res.status(200).json(planets)
+async function getAllPlanets (req, res) {
+  return res.status(200).json((await planets.find({}, { _id: 0, __v: 0 })))
 }
 
 module.exports = {
