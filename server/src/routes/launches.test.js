832b3ca0c7ee413c55b7b7d0c970e46eec43d98f
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-10-26 00:02:44 +0400

    refactor tests and add new ones

--- server/src/routes/launches.test.js
+++ server/src/routes/launches.test.js
@@ -1,78 +1,91 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('test GET /launches', () => {
-  test('it should return 200 status code', async () => {
-    const response = await request(app).get('/launches')
-      .expect('Content-type', /json/)
-      .expect(200)
-  })
-})
-
-describe('test POST /launches', () => {
-  const fakeData = {
-    rocket: 'Azerspace',
-    launchDate: new Date('February 23,2022'),
-    mission: 'exploring Mars',
-    target: 'kapler 101'
-  }
-
-  test('it should return 201 status code', async () => {
-    await request(app).post('/launches')
-      .send(fakeData)
-      .expect(201)
+const {
+  connectMongodb,
+  disconnectMongodb
+} = require('../../data/config/mongo.connection')
+describe('Launches tests', () => {
+  beforeAll(async () => {
+    await connectMongodb('mongodb://localhost:27017')
   })
 
-  test('it should return a json success message', async () => {
-    const response = await request(app).post('/launches')
-      .send(fakeData)
-
-    expect(response.body).toMatchObject({
-      message: 'saved'
+  afterAll(async () => {
+    await disconnectMongodb()
+  })
+  describe('test GET /launches', () => {
+    test('it should return 200 status code', async () => {
+      const response = await request(app).get('/launches')
+        .expect('Content-type', /json/)
+        .expect(200)
     })
   })
 
-  test('it should return an error message and 400 status code when one of the field is missed', async () => {
-    const response = await request(app).post('/launches')
-      .send({})
-      .expect(400)
+  describe('test POST /launches', () => {
+    const fakeData = {
+      rocket: 'Azerspace',
+      launchDate: new Date('February 23,2022'),
+      mission: 'exploring Mars',
+      target: 'kapler 101'
+    }
 
-    expect(response.body).toMatchObject({
-      error: 'one of the launch properties is missed'
+    test('it should return 201 status code', async () => {
+      await request(app).post('/launches')
+        .send(fakeData)
+        .expect(201)
     })
-  })
 
-  test('it should return an error and 400 code when date format given in false format', async () => {
-    const response = await request(app)
-      .post('/launches')
-      .send({
-        rocket: 'Azerspace',
-        launchDate: 'tghytjuyj',
-        mission: 'exploring Mars',
-        target: 'kapler 101'
+    test('it should return a json success message', async () => {
+      const response = await request(app).post('/launches')
+        .send(fakeData)
+
+      expect(response.body).toMatchObject({
+        message: 'saved'
       })
-      .expect(400)
-
-    expect(response.body).toMatchObject({
-      error: 'invalid date format'
     })
-  })
 
-  // test('it should return 200 status code when launch is deleted', async () => {
-  //   await request(app)
-  //     .delete('/launches')
-  //     .send({ id: 101 })
-  //     .expect(200)
-  // })
+    test('it should return an error message and 400 status code when one of the field is missed', async () => {
+      const response = await request(app).post('/launches')
+        .send({})
+        .expect(400)
 
-  test('it should return 400 status code and error message when given id is not found', async () => {
-    const response = await request(app)
-      .delete('/launches')
-      .send({ id: 233 })
-      .expect(400)
+      expect(response.body).toMatchObject({
+        error: 'one of the launch properties is missed'
+      })
+    })
 
-    expect(response.body).toMatchObject({
-      message: 'this launch does not  exist'
+    test('it should return an error and 400 code when date format given in false format', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send({
+          rocket: 'Azerspace',
+          launchDate: 'tghytjuyj',
+          mission: 'exploring Mars',
+          target: 'kapler 101'
+        })
+        .expect(400)
+
+      expect(response.body).toMatchObject({
+        error: 'invalid date format'
+      })
+    })
+
+    // test('it should return 200 status code when launch is deleted', async () => {
+    //   await request(app)
+    //     .delete('/launches')
+    //     .send({ id: 101 })
+    //     .expect(200)
+    // })
+
+    test('it should return 400 status code and error message when given id is not found', async () => {
+      const response = await request(app)
+        .delete('/launches')
+        .send({ id: 233 })
+        .expect(400)
+
+      expect(response.body).toMatchObject({
+        message: 'this launch does not  exist'
+      })
     })
   })
 })
